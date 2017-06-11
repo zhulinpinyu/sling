31500a2c15855b8a8e49d98568c2ab9b752a454f
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-11 11:50:58 +0800

    Backend JWT Auth

--- api/lib/sling/guardian_serializer.ex
+++ api/lib/sling/guardian_serializer.ex
@@ -4,8 +4,8 @@ defmodule Sling.GuardianSerializer do
   alias Sling.Repo
   alias Sling.User
 
-  def from_token(user = %User{}), do: {:ok, "User: #{user.id}"}
-  def from_token(_), do: {:error, "Unknown resource type"}
+  def for_token(user = %User{}), do: {:ok, "User: #{user.id}"}
+  def for_token(_), do: {:error, "Unknown resource type"}
 
   def from_token("User:" <> id), do: {:ok, Repo.get(User, String.to_integer(id))}
   def from_token(_), do: {:error, "Unknown resource type"}
--- api/web/router.ex
+++ api/web/router.ex
@@ -3,9 +3,17 @@ defmodule Sling.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.Plug.LoadResource
   end
 
   scope "/api", Sling do
     pipe_through :api
+
+    post "/sessions", SessionController, :create
+    delete "/sessions", SessionController, :delete
+    post "/sessions/refresh", SessionController, :refresh
+    resources "/users", UserController, only: [:create]
+
   end
 end
