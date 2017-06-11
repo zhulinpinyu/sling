c54116cf42107ecd2cd837d6f15c65cd0efbabe8
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-11 22:28:51 +0800

    更新首页添加相应的登录注册Link

--- web/src/containers/Home/index.js
+++ web/src/containers/Home/index.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router'
+import { connect } from 'react-redux'
+
+import { logout } from '../../actions/session'
+import Navbar from '../../components/Navbar'
 
 class Home extends Component {
+  static contextTypes = {
+    router: PropTypes.object.isRequired
+  }
+
+  static propTypes = {
+    logout: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired,
+    currentUser: PropTypes.object.isRequired
+  }
+
+  handleLogout = () => {
+    this.props.logout(this.context.router)
+  }
+
   render(){
+    const { isAuthenticated, currentUser } = this.props
     return (
-      <div>
-        Home
+      <div style={{ flex: '1' }}>
+        <Navbar />
+        <ul>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Signup</Link></li>
+        </ul>
+        { isAuthenticated &&
+          <div>
+            <span>{currentUser.username}</span>
+            <button type="button" onClick={this.handleLogout}>Logout</button>
+          </div>
+        }
       </div>
     )
   }
 }
 
-export default Home
+const mapStateToProps = ({ session }) => {
+  return {
+    isAuthenticated: session.isAuthenticated,
+    currentUser: session.currentUser
+  }
+}
+
+
+export default connect(mapStateToProps, { logout })(Home)
