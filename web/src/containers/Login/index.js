4056523d236d0af2adba52075e05ffd64c602565
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-11 16:13:36 +0800

    完善Login

--- web/src/containers/Login/index.js
+++ web/src/containers/Login/index.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import Navbar from '../../components/Navbar'
+import LoginForm from '../../components/LoginForm'
 
 class Login extends Component {
+  static contextTypes = {
+    router: PropTypes.shape.isReqiured
+  }
+
+  // static propTypes = {
+  //   login: PropTypes.func.isReqiured
+  // }
+
+  handleLogin = () => {
+    // this.props.login(data, this.context.router)
+  }
+
   render() {
     return (
-      <div>Login</div>
+      <div style={{ flex: 1 }}>
+        <Navbar />
+        <LoginForm onSubmit={this.handleSubmit} />
+      </div>
     )
   }
 }
