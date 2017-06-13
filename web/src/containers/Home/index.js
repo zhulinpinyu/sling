131c2ddede8e81ec82d7b38e10f6a63eca563642
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-13 14:00:54 +0800

    [web] 更新重构 Home container

--- web/src/containers/Home/index.js
+++ web/src/containers/Home/index.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router'
 import { connect } from 'react-redux'
+import { css, StyleSheet } from 'aphrodite'
 
-import { logout } from '../../actions/session'
+import {
+  fetchRooms,
+  createRoom,
+  joinRoom
+} from '../../actions/rooms'
 import Navbar from '../../components/Navbar'
+import RoomListItem from '../../components/RoomListItem'
+import NewRoomForm from '../../components/NewRoomForm'
+
+const styles = StyleSheet.create({
+  card: {
+    maxWidth: '500px',
+    padding: '3rem 4rem',
+    margin: '2rem auto'
+  }
+})
 
 class Home extends Component {
   static contextTypes = {
@@ -12,41 +26,68 @@ class Home extends Component {
   }
 
   static propTypes = {
-    logout: PropTypes.func.isRequired,
-    isAuthenticated: PropTypes.bool.isRequired,
-    currentUser: PropTypes.object.isRequired
+    joinRoom: PropTypes.func.isRequired,
+    createRoom: PropTypes.func.isRequired,
+    fetchRooms: PropTypes.func.isRequired,
+    currentUserRooms: PropTypes.array,
+    rooms: PropTypes.array
   }
 
-  handleLogout = () => {
-    this.props.logout(this.context.router)
+  static defaultProps = {
+    currentUserRooms: [],
+    rooms: []
+  }
+
+  componentDidMount(){
+    this.props.fetchRooms()
+  }
+
+  handleNewRoomSubmit = (data) => {
+    this.props.createRoom(data, this.context.router)
+  }
+
+  handleRoomJoin = roomId => this.props.joinRoom(roomId, this.context.router)
+
+  renderRooms() {
+    const currentUserRoomIds = this.props.currentUserRooms.map(room => room.id)
+    return this.props.rooms.map((room) => {
+      return (
+        <RoomListItem
+          key={room.id}
+          room={room}
+          onRoomJoin={this.handleRoomJoin}
+          currentUserRoomIds={currentUserRoomIds}
+        />
+      )
+    })
   }
 
   render(){
-    const { isAuthenticated, currentUser } = this.props
     return (
       <div style={{ flex: '1' }}>
         <Navbar />
-        <ul>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/signup">Signup</Link></li>
-        </ul>
-        { isAuthenticated &&
-          <div>
-            <span>{currentUser.username}</span>
-            <button type="button" onClick={this.handleLogout}>Logout</button>
-          </div>
-        }
+        <div className={`card ${css(styles.card)}`}>
+          <h3 style={{ marginBottom: '2rem', textAlign: 'center' }}>Create a new room</h3>
+          <NewRoomForm onSubmit={this.handleNewRoomSubmit} />
+        </div>
+        <div className={`card ${css(styles.card)}`}>
+          <h3 style={{ marginBottom: '2rem', textAlign: 'center' }}>Join a room</h3>
+          {this.renderRooms()}
+        </div>
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ session }) => {
+const mapStateToProps = ({ rooms }) => {
   return {
-    isAuthenticated: session.isAuthenticated,
-    currentUser: session.currentUser
+    rooms: rooms.all,
+    currentUserRooms: rooms.currentUserRooms
   }
 }
 
-
-export default connect(mapStateToProps, { logout })(Home)
+export default connect(mapStateToProps, {
+  fetchRooms,
+  createRoom,
+  joinRoom
+})(Home)
