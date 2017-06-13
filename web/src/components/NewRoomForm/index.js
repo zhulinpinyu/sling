286ef5de313a5dc9d57c1af8d61eeccc1fcd70f4
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-13 12:17:41 +0800

    NewRoomForm component

---
+++ web/src/components/NewRoomForm/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Field, reduxForm } from 'redux-form'
+
+class NewRoomForm extends Component {
+
+  static propTypes = {
+    onSubmit: PropTypes.func,
+    handleSubmit: PropTypes,
+    submitting: PropTypes.bool.isRequired
+  }
+
+  static defaultProps = {
+    onSubmit: () => {},
+    handleSubmit: () => {}
+  }
+
+  handleSubmit = data => this.props.onSubmit(data)
+
+  render(){
+    const { handleSubmit, submitting } = this.props
+    return (
+      <form onSubmit={handleSubmit(this.handleSubmit)}>
+        <div className="input-group">
+          <Field
+            type="text"
+            name="name"
+            placeholder="name"
+            component="input"
+            className="form-control"
+          />
+          <div className="input-group-btn">
+            <button type="submit" className="btn btn-primsry" disabled={submitting}>
+              { submitting ? 'Saving...' : 'Submit' }
+            </button>
+          </div>
+        </div>
+      </form>
+    )
+  }
+}
+
+const validate = (values) => {
+  const errors = {}
+  if(!values.name){
+    errors.name = 'Required'
+  }
+  return errors
+}
+
+export default reduxForm({
+  form: 'newRoom',
+  validate
+})(NewRoomForm)
