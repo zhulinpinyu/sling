cce49f213fe0178e74c39bab1a361d676adbb147
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-14 16:02:52 +0800

    Message Form 组件

---
+++ web/src/components/MessageForm/index.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Field, reduxForm } from 'redux-form'
+import { css, StyleSheet } from 'aphrodite'
+
+const styles = StyleSheet.create({
+  form: {
+    padding: '0px 10px 10px 10px',
+    background: '#fff'
+  },
+  input: {
+    borderWidth: '2px',
+    borderColor: 'rgb(214,214,214)'
+  },
+  button: {
+    color: 'rgb(80,80,80)',
+    borderWidth: '2px',
+    borderColor: 'rgb(214,214,214)',
+    background: 'rgb(214,214,214)'
+  }
+})
+
+class MessageForm extends Component {
+
+  static propTypes = {
+    onSubmit: PropTypes.func,
+    handleSubmit: PropTypes.func,
+    submitting: PropTypes.bool.isRequired
+  }
+
+  static defaultProps = {
+    onSubmit: () => {},
+    handleSubmit: () => {}
+  }
+
+  handleSubmit = data => this.props.onSubmit(data)
+
+  render(){
+    const { handleSubmit, submitting } = this.props
+    return (
+      <form onSubmit={handleSubmit(this.handleSubmit)} className={css(styles.form)}>
+        <div className="input-group">
+          <Field
+            type="text"
+            name="text"
+            placeholder="message"
+            component="input"
+            className={`form-control ${styles.input}`}
+          />
+          <div className="input-group-btn">
+            <button className={`btn ${css(styles.button)}`} disabled={submitting}>
+              Send
+            </button>
+          </div>
+        </div>
+      </form>
+    )
+  }
+}
+
+const validate = (values) => {
+  const errors = {}
+  if(!values.text){
+    errors.text = 'Required'
+  }
+  return errors
+}
+
+export default reduxForm({
+  form: 'newMessage',
+  validate
+})(MessageForm)
