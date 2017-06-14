b0614c2f07bab9afe556eff88348f3eea0901698
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-14 16:45:53 +0800

    MessageList 组件

---
+++ web/src/components/MessageList/index.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import _ from 'lodash'
+import moment from 'moment'
+import { css, StyleSheet } from 'aphrodite'
+
+import Message from '../Message'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: '1',
+    padding: '10px 10px 0 10px',
+    background: '#fff',
+    overflowY: 'auto'
+  },
+  dayDivider: {
+    position: 'relative',
+    margin: '1rem 0',
+    textAlign: 'center',
+    '::after': {
+      position: 'absolute',
+      top: '50%',
+      right: '0',
+      left: '0',
+      height: '1px',
+      background: 'rgb(240,240,240)',
+      content: '""'
+    }
+  },
+  dayText: {
+    zIndex: 1,
+    background: '#fff',
+    position: 'relative',
+    padding: '0 12px'
+  }
+})
+
+class MessageList extends Component {
+  static propTypes = {
+    messages: PropTypes.array
+  }
+
+  static defaultProps = {
+    messages: []
+  }
+
+  renderMessages(messages){
+    return messages.map((message) => {
+      return (
+        <Message key={message.id} message={message} />
+      )
+    })
+  }
+
+  renderDays(){
+    const messages = this.props.messages
+      .map((m) => {
+        return {
+          ...m,
+          day: moment(m.inserted_at).format('MMMM Do')
+        }
+      })
+    const dayGroups = _.groupBy(messages, 'day')
+    const days = []
+    _.mapKeys(dayGroups, (value, key) => {
+      days.push({ date: key, messages: value })
+    })
+    const today = moment().format('MMMM Do')
+    const yestoday = moment().subtrcat(1, 'days').format('MMMM Do')
+    return days.map((day) => {
+      return (
+        <div key={day.date}>
+          <div className={css(styles.dayDivider)}>
+            <span className={css(styles.dayText)}>
+              {day.date === today && 'Today'}
+              {day.date === yestoday && 'Yestoday'}
+              {![today, yestoday].includes(day.date) && day.date}
+            </span>
+          </div>
+          {this.renderMessages(day.messages)}
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div className={css(styles.container)}>
+        {this.renderDays()}
+      </div>
+    )
+  }
+}
+
+export default MessageList
