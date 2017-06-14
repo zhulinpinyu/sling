8db606d8c938dc11d3be42309fc49e158b4c60de
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-14 11:42:53 +0800

    message 数据分页

--- api/web/channels/room_channel.ex
+++ api/web/channels/room_channel.ex
@@ -3,9 +3,19 @@ defmodule Sling.RoomChannel do
 
   def join("room:" <> room_id, _params, socket) do
     room = Repo.get!(Sling.Room, room_id)
+
+    page = Sling.Message
+            |> where([m], m.id == ^room.id)
+            |> order_by(desc: :inserted_at, desc: :id)
+            |> preload(:user)
+            |> Sling.Repo.paginate()
+
     response = %{
       room: Phoenix.View.render_one(room, Sling.RoomView, "room.json")
+      messages: Phoenix.View.render_many(page.enties, Sling.MessageView, "message.json"),
+      pagination: Sling.PageinationHelpers.pagination(page)
     }
+
     {:ok, response, assign(socket, :room, room)}
   end
 
